c04f3200e50a5f9d8a50a82687031c97c701b3a1
chaeyk <chaeyk@gmail.com>
2022-10-12 00:51:23 +0900

    working with vs2022 / .net6 / ix token

--- Nethereum.Erc20.Blazor/Models/ERC20ContractModel.cs
+++ Nethereum.Erc20.Blazor/Models/ERC20ContractModel.cs
@@ -7,10 +7,10 @@ namespace Nethereum.Erc20.Blazor
     public class ERC20ContractModel
     {
         public const int DEFAULT_DECIMALS = 18;
-        public const string DAI_SMART_CONTRACT = "0x6b175474e89094c44da98b954eedeac495271d0f";
+        public const string IX_SMART_CONTRACT = "0x123455D0f27E9e6c4447322524d6f2Acd22f340A";
         public ERC20ContractModel() { }
 
-        public string Address { get; set; } = DAI_SMART_CONTRACT;
+        public string Address { get; set; } = IX_SMART_CONTRACT;
         public int DecimalPlaces { get; set; } = DEFAULT_DECIMALS;
         public string Name { get; set; }
     }
--- Nethereum.Erc20.Blazor/Models/ERC20TransferModel.cs
+++ Nethereum.Erc20.Blazor/Models/ERC20TransferModel.cs
@@ -6,13 +6,14 @@ namespace Nethereum.Erc20.Blazor
     public class ERC20TransferModel
     {
         public ERC20ContractModel ERC20Contract { get; set; } = new ERC20ContractModel();
-        public string To { get; set; }  
+        public string To { get; set; } = "0x156bc2186c73F4960b916cd554842F69321dD53C";
         public decimal Value { get; set; }
 
-        public TransferFunction GetTransferFunction()
+        public TransferFunction GetTransferFunction(string from)
         {
             return new TransferFunction()
             {
+                FromAddress = from,
                 To = To,
                 Value = Web3.Web3.Convert.ToWei(Value, ERC20Contract.DecimalPlaces),
                 AmountToSend = 0
